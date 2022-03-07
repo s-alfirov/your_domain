06bfd57e02cf0ef4e18b1ff6697a0f031887abff
sergej <sergej2222@googlemail.com>
2022-03-07 18:41:54 +0100

    zweite

---
+++ index.php
@@ -0,0 +1,12 @@
+<html lang="de">
+    <head>
+        <title>Welcome to Your_domain!</title>
+
+    </head>
+    <body>
+        <h1>Success!  The your_domain virtual host is working!</h1>
+
+        <?php
+            phpinfo();?>
+    </body>
+</html>
